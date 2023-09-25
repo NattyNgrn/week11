936ea828f7d75e5549bd35efcf4702257eadfff7
NattyNgrn <natnrgn@gmail.com>
2023-09-25 00:02:16 -0400

    set up main branch

---
+++ client/src/components/addnew.jsx
@@ -0,0 +1,38 @@
+function Addnew() {
+  function save(){
+
+  }
+
+  function cancel(){
+    
+  }
+
+    return (
+        <div>
+
+          <form>
+            <lable>
+              Name: <input/>
+            </lable>
+
+            <label>
+              Email: <input/>
+            </label>
+
+            <label>
+              Phone Number: <input/>
+            </label>
+
+            <label>
+              Notes: <input/>
+            </label>
+
+            <button>Cancel</button>
+            <button>Save</button>
+          </form>
+
+        </div>
+    )
+};
+
+export default Addnew;
