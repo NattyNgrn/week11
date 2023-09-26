69375ca45bf4777274bee7759e0834bd56b88326
NattyNgrn <natnrgn@gmail.com>
2023-09-25 22:20:52 -0400

    wokring

--- client/src/components/addnew.jsx
+++ client/src/components/addnew.jsx
@@ -11,27 +11,54 @@ function Addnew() {
         <div>
 
           <form>
-            <lable>
-              Name: <input/>
-            </lable>
+            <div class="flex flex-col items-center">
+            <label for="name" class="text-sm font-medium text-gray-700">Name</label>
+            <input type="text" id="name" class="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent" placeholder="Name"/>
 
-            <label>
-              Email: <input/>
-            </label>
+            <label for="email" class="text-sm font-medium text-gray-700">Email</label>
+            <input type="email" id="email" class="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent" placeholder="Email"/>
 
-            <label>
-              Phone Number: <input/>
-            </label>
+            <label for="phone" class="text-sm font-medium text-gray-700">Phone number</label>
+            <input type="tel" id="phone" class="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent" placeholder="Phone number"/>
 
-            <label>
-              Notes: <input/>
-            </label>
+            <label for="notes" class="text-sm font-medium text-gray-700">Notes</label>
+            <textarea id="notes" class="border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent" placeholder="Notes"></textarea>
 
-            <button>Cancel</button>
-            <button>Save</button>
-          </form>
+            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+                Cover photo
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>Upload a file</span>
+                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                    </label>
+                    <p className="pl-1">or drag and drop</p>
+                  </div>
+                  <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                </div>
+                </div>
 
+
+            <div className="col-span-full">
+              
+            <div class="flex justify-center mt-4">
+              <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-md mr-2">Cancel</button>
+              <button class="bg-indigo-600 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-md">Save</button>
+            </div>
+
+
+            </div>
+
+            
+            </div>
+        </form>
         </div>
+
     )
 };
 
--- client/src/components/allcontacts.jsx
+++ client/src/components/allcontacts.jsx
@@ -1,7 +1,9 @@
+import Addnew from "./addnew"
+
 function AllContacts(){
 
 return (
-    <div class="grid-cols-1 sm:grid md:grid-cols-2 ">
+    <div class="grid-cols-1 sm:grid md:grid-cols-4 ">
     <div
         class="mx-3 mt-6 flex flex-col self-start rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 sm:shrink-0 sm:grow sm:basis-0">
         <a href="#!">
@@ -56,8 +58,7 @@ return (
             Card title
         </h5>
         <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content.
+        This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
         </p>
         </div>
     </div>
@@ -85,4 +86,4 @@ return (
     )
 }
 
-export default AllContacts;
+export default Addnew
